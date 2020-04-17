191ffa943c753fb12b89bc7d96220ecdb56a0907
Lukman Bayhaqi <lukmanbaihaqi78@gmail.com>
2020-04-17 13:51:04 +0700

    create test register

--- test/login.test.js
+++ test/login.test.js
@@ -9,19 +9,22 @@ describe('Login test section', _ => {
       username: 'teacher',
       email: 'teacher@mail.com',
       password: '12345',
-      role: 'teacher'
+      role: 'teacher',
+      phoneNumber: '081234432180'
     }
     const parent = {
       username: 'parent',
       email: 'parent@mail.com',
       password: '12345',
-      role: 'parent'
+      role: 'parent',
+      phoneNumber: '081234432180'
     }
     const superAdmin = {
       username: 'superAdmin',
       email: 'superAdmin@mail.com',
       password: '12345',
-      role: 'superAdmin'
+      role: 'superAdmin',
+      phoneNumber: '081234432180'
     }
     const _ = await User.create(teacher)
     const _ = await User.create(parent)
@@ -32,92 +35,82 @@ describe('Login test section', _ => {
     const _ = await queryInterface.bulkDelete('Users', null, {})
     done()
   })
-
-  describe('/login section', _ => {
-    describe('success response, will returning status code 200, token and message', _ => {
-      test('Teachers login', (done) => {
-        request(app)
-          .post('/login')
-          .send({
-            email: 'teacher@mail.com', //email gausah unique
-            password: '12345', //password minimal 5
-            role: 'teacher' //hari sabtu diomongin
-          })
-          .end((err, { status, body }) => {
-            expect(err).toBeNull()
-            expect(status).toBe(200)
-            expect(body).toHaveProperty('token')
-            expect(body.message).toBe('Success login as teacher') // teacher name
-            done()
-          })
-      })
-      test('Parents Login', (done) => {
-        request(app)
-          .post('/login')
-          .send({
-            email: 'parent@mail.com',
-            password: '12345',
-            role: 'parent'
-          })
-          .end((err, { status, body }) => {
-            expect(err).toBeNull()
-            expect(status).toBe(200)
-            expect(body).toHaveProperty('token')
-            expect(body.message).toBe('Success login as parent')
-            done()
-          })
-      })
-      test('SuperAdmin Login', (done) => {
-        request(app)
-          .post('/login')
-          .send({
-            email: 'superAdmin@mail.com',
-            password: '12345',
-            role: 'superAdmin'
-          })
-          .end((err, { status, body }) => {
-            expect(err).toBeNull()
-            expect(status).toBe(200)
-            expect(body).toHaveProperty('token')
-            expect(body.message).toBe('Success login as superAdmin')
-            done()
-          })
-      })
+  
+  describe('success response, will returning status code 200, token and message', _ => {
+    test('Teachers login', (done) => {
+      request(app)
+        .post('/login')
+        .send({
+          email: 'teacher@mail.com',
+          password: '12345'
+        })
+        .end((err, { status, body }) => {
+          expect(err).toBeNull()
+          expect(status).toBe(200)
+          expect(body).toHaveProperty('token')
+          expect(body.message).toBe('Success login as teacher') // teacher name
+          done()
+        })
     })
-    describe('Error response', _ => {
-      test('Because email invalid', done => {
-        request(app)
-          .post('/login')
-          .send({
-            username: 'teacher',
-            email: 'teach@mail.com',
-            password: '12345',
-            role: 'teacher'
-          })
-          .end((err, { status, body }) => {
-            expect(err).toBeNull()
-            expect(status).toBe(400)
-            expect(body.message).toBe('Invalid email / password')
-            done()
-          })
-      })
-      test('Because password invalid', done => {
-        request(app)
-          .post('/login')
-          .send({
-            username: 'teacher',
-            email: 'teacher@mail.com',
-            password: '123456',
-            role: 'teacher'
-          })
-          .end((err, { status, body }) => {
-            expect(err).toBeNull()
-            expect(status).toBe(400)
-            expect(body.message).toBe('Invalid email / password')
-            done()
-          })
-      })
+    test('Parents Login', (done) => {
+      request(app)
+        .post('/login')
+        .send({
+          email: 'parent@mail.com',
+          password: '12345'
+        })
+        .end((err, { status, body }) => {
+          expect(err).toBeNull()
+          expect(status).toBe(200)
+          expect(body).toHaveProperty('token')
+          expect(body.message).toBe('Success login as parent')
+          done()
+        })
+    })
+    test('SuperAdmin Login', (done) => {
+      request(app)
+        .post('/login')
+        .send({
+          email: 'superAdmin@mail.com',
+          password: '12345'
+        })
+        .end((err, { status, body }) => {
+          expect(err).toBeNull()
+          expect(status).toBe(200)
+          expect(body).toHaveProperty('token')
+          expect(body.message).toBe('Success login as superAdmin')
+          done()
+        })
     })
   })
-
+  describe('Error response', _ => {
+    test('Because email invalid', done => {
+      request(app)
+        .post('/login')
+        .send({
+          email: 'teach@mail.com',
+          password: '12345'
+        })
+        .end((err, { status, body }) => {
+          expect(err).toBeNull()
+          expect(status).toBe(400)
+          expect(body.message).toBe('Invalid email / password')
+          done()
+        })
+    })
+    test('Because password invalid', done => {
+      request(app)
+        .post('/login')
+        .send({
+          email: 'teacher@mail.com',
+          password: '123456'
+        })
+        .end((err, { status, body }) => {
+          expect(err).toBeNull()
+          expect(status).toBe(400)
+          expect(body.message).toBe('Invalid email / password')
+          done()
+        })
+    })
+  })
 })

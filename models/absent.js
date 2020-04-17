a0c237b638ed0b30de6ef25adcf8af0da79be12b
reinfajar <tsanimfajar@outlook.com>
2020-04-17 22:24:27 +0700

    validate

--- models/absent.js
+++ models/absent.js
@@ -3,9 +3,27 @@ module.exports = (sequelize, DataTypes) => {
   class Absent extends sequelize.Sequelize.Model {}
 
   Absent.init({
-    StudentId: DataTypes.INTEGER,
-    absentDate: DataTypes.DATE,
-    status: DataTypes.STRING
+    StudentId: {
+      type : DataTypes.INTEGER,
+      allowNull : false,
+      validate : {
+        notNull: { args: true, msg: 'StudentId Cannot Null' }
+      }
+    },
+    absentDate: {
+      type : DataTypes.DATE,
+      allowNull : false,
+      validate : {
+        notNull: { args: true, msg: 'Date Cannot Null' }
+      }
+    },
+    status: {
+      type : DataTypes.STRING,
+      allowNull : false,
+      validate : {
+        notNull: { args: true, msg: 'Status Cannot Null' }
+      }
+    }
   }, {
     sequelize
   })
--- models/parent.js
+++ models/parent.js
@@ -1,14 +1,47 @@
 'use strict';
+const Helper = require('../helpers/helper');
+
 module.exports = (sequelize, DataTypes) => {
   class Parent extends sequelize.Sequelize.Model {}
 
   Parent.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    phoneNumber: DataTypes.STRING
+    name: {
+      type : DataTypes.STRING,
+      allowNull : false,
+      validate : {
+        notNull: { args: true, msg: 'Name Cannot Null' }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: { args: true, msg: 'Invalid Email Address' },
+        notNull: { args: true, msg: 'Email Cannot Null' }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { args: true, msg: 'Password Cannot Null' },
+        len: { args: [5, 20], msg: 'Password Length Should Be 5-20 Length' }
+      }
+    },
+    phoneNumber: {
+      type : DataTypes.STRING,
+      allowNull : false,
+      validate : {
+        len: { args: [9, 13], msg: 'Phone Number Length Should Be 9-13 Length' }
+      }
+    }
   }, {
-    sequelize
+    sequelize,
+    hooks: {
+      beforeCreate(user, options) {
+        user.password = Helper.hashPassword(user.password);
+      }
+    }
   })
 
   Parent.associate = function(models) {

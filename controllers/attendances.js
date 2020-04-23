02c441303d9265ff7b62baf88fff0781d481e8a9
Lukman Bayhaqi <lukmanbaihaqi78@gmail.com>
2020-04-23 10:01:37 +0700

    remove console.log

--- controllers/attendances.js
+++ controllers/attendances.js
@@ -3,7 +3,6 @@ const { getRedis, setRedis, deleteRedis } = require('../helpers')
 
 module.exports = {
   create(req, res, next) {
-    console.log(`masuk controller`);
     const { data } = req.body
     const { AttendanceId } = req.headers
     let promiseAll = []
